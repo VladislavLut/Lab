f1517524d55d9955326d3df3567bf27338d1e28e
blxckvay <revatski64@gmail.com>
2023-09-19 20:24:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+n =int(input('Введіть число: '))
+
+if 0<=n<=6:
+    print("Good Night")
+
+elif 6<=n<=13:
+    print("Good Morning")
+
+elif 13<=n<=17:
+    print("Good Day")
+
+elif 17<=n<=0:
+    print("Good Evening")
+
+else:
+    print("Невірне число")
+
